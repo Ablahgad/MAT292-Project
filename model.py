354c9dd6210c553a54db3898a14db856d3bde15e
cdannisb <22ciara2@gmail.com>
2025-11-13 18:55:38 -0500

    added input variables

--- model.py
+++ model.py
@@ -1,8 +1,13 @@
-#input section - real domain dimensions, number of nodes along x, number of nodes along y
+#input section - real domain dimensions (mm), number of nodes along x, number of nodes along y
+domain_x = 150
+domain_y = 45.7
 
+divisions_x = 10
+divisions_y = 10
 
+#n
 
-#need to define a square
 #need inputs 
 #need a total number of nodes
 #store total number of nodes in a matrix
+
