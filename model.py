1e108add65e4f4e375cc6c169f784ed7bd0d9da4
cdannisb <22ciara2@gmail.com>
2025-11-13 18:46:27 -0500

    Update model.py

---
+++ model.py
@@ -0,0 +1,8 @@
+#input section - real domain dimensions, number of nodes along x, number of nodes along y
+
+
+
+#need to define a square
+#need inputs 
+#need a total number of nodes
+#store total number of nodes in a matrix
