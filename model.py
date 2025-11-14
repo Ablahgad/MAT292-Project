c320b4816d71cdf00c5ccfb33d1ddd1182375f60
Ablah <115513366+Ablahgad@users.noreply.github.com>
2025-11-14 10:54:54 -0500

    Coded in motion of the fish and updating the nodes to reflect the motion

--- model.py
+++ model.py
@@ -78,37 +78,39 @@ def f_y(s, thickness):
 # L_x = [L_nodes[i].x for i in range(len(L_nodes))]
 # L_y = [L_nodes[i].y for i in range(len(L_nodes))]
 
+scale_modifier = thickness/10
 
 def y_tail(x, t):
-    return (2.7826*x - 0.1485*x**2)*math.sin(2*math.pi*(x/29.766+0.25/t))
+    return scale_modifier*(2.7826*x - 0.1485*x**2)*math.sin(2*math.pi*(x/29.766+0.25*t))+thickness/4
 
 head = -4.5
 tail = 9
 thickness = 1
-file_text = ""
 
-for node in L_nodes:
-    s_i = f_s(node.x, head, tail)
-    if s_i < 1 and s_i > 0:
-        y_i = f_y(s_i, thickness)
-        if node.y>(-1)*y_i and node.y<y_i:
-            node.chi = 1
+dt = 1 #time step for movement of fish tail
+# t = 0.01
+
+for t in range(1, 5, dt):
+
+    file_text = ""
+
+    for node in L_nodes:
+        s_i = f_s(node.x, head, tail)
+        if s_i < 1 and s_i > 0:
+            if node.x > 0:
+                y_i = y_tail(node.x, t)
+                y_i_bot =  y_tail(node.x, t)-f_y(s_i, thickness)-thickness/4
+            else:
+                y_i = f_y(s_i, thickness)
+                y_i_bot = -1*f_y(s_i, thickness)
+            if node.y>y_i_bot and node.y<y_i:
+                node.chi = 1
+            else:
+                node.chi = 0
         else:
             node.chi = 0
-    else:
-        node.chi = 0
 
-    file_text += str(node)
+        file_text += str(node)
 
-with open("nodes.txt", "w") as file:
-    file.write(file_text)
-
-
-#Starting to work on movement of tail
-
-# dt = 1 #time step for movement of fish tail
-# t = 0.01
-# for node in L_nodes:
-#     if node.x > 0 and node.x<tail+head:
-#         y_i = y_tail(node.x, t)
-#
+    with open("nodes" +str(t)+ ".txt", "w") as file:
+        file.write(file_text)
