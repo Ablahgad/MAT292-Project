74b53b64c64da671cc128687f3ebfa4fda1dacd4
cdannisb <22ciara2@gmail.com>
2025-11-13 20:00:59 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+#import libraries
+import numpy as np
+
 #input section - real domain dimensions (mm), number of nodes along x, number of nodes along y
 domain_x = 150
 domain_y = 45.7
@@ -6,10 +9,22 @@ divisions_x = 10
 divisions_y = 10
 
 #create matrix with x and y intersections (nodal locations)
-for i in 
+nodes = np.zeros(((divisions_x * divisions_y), 2))
 
-#need inputs 
-#need a total number of nodes
-#store total number of nodes in a matrix
+#initialize loop variables
+index = 0
+d_x = (domain_x/(divisions_x - 1))
+d_y = (domain_y/(divisions_y - 1))
 
+#loop through all columns
+for i in range(divisions_x):
+    #loop through all rows
+    for j in range(divisions_y):
+        x = i * d_x
+        y = j * d_y
+        nodes[index] = (x,y)
+        index += 1
 
+#print values in matrix
+print(nodes)
+print(nodes.shape)
